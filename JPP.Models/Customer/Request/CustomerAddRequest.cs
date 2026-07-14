9ee5d341c655f4430b6e829875912baab103ac01
Radianda Setiawan <setiawanradianda@gmail.com>
2026-07-14 15:55:16 +0700

    addcustomer

---
+++ JPP.Models/Customer/Request/CustomerAddRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.Models.Customer.Request
+{
+    public class CustomerAddRequest
+    {
+        public int ID { get; set; }
+        public int UID { get; set; }
+    }
+}
